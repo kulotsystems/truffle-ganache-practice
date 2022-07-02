f7e640fff9f6596cd3ffe43d0e8f4eafaa4a64c8
Arvic S. Babol <kulotsystems@gmail.com>
2022-07-02 12:29:57 +0800

    Modify contract.

--- contracts/Kulot.sol
+++ contracts/Kulot.sol
@@ -5,6 +5,10 @@ pragma solidity >=0.8.7;
 contract Kulot {
     uint age;
 
+    constructor(uint _age) {
+        age = _age;
+    }
+
     function setAge(uint _age) public {
         age = _age;
     }
